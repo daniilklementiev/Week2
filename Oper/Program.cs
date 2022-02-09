c1e6b0b78ef634ce3cb095fa0d5a13e1fac1de06
niglelxrd <klementevdaniil@gmail.com>
2022-02-09 23:17:22 +0200

    123123

--- Oper/Program.cs
+++ Oper/Program.cs
@@ -7,6 +7,37 @@ namespace Oper
         static void Main(string[] args)
         {
             Console.WriteLine("Operations");
+            Console.CursorVisible = false;
+            Console.WriteLine("Loading... ");
+            int pos = Console.CursorTop;
+            Console.SetCursorPosition(11, 0);
+            Console.Write("OK");
+            Console.SetCursorPosition(0, pos);
+
+            var rand = new Random();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i < 25)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (i < 50)
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                else if (i < 75)
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                else if (i < 100)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                string pct = string.Format("{0,-30} {1,3}%", new string((char)0x2592, i * 30 / 100), i);
+                Console.CursorLeft = 0;
+                Console.Write(pct);
+                Thread.Sleep(rand.Next(0, 100));
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
             /*var c0 = new DataTypes.Complex { Re = 0, Im = 0 };
             var c1 = new DataTypes.Complex { Re = 1, Im = -2 };
             var c2 = new DataTypes.Complex { Re = 2, Im = -3 };
@@ -32,14 +63,11 @@ namespace Oper
                 Console.WriteLine("Неизвестное исключение: " + ex.Message);
             }*/
 
-           // var f0 = new DataTypes.Fraction { Denominator = 0, Numerator = 1 };
+            // var f0 = new DataTypes.Fraction { Denominator = 0, Numerator = 1 };
             var f1 = new DataTypes.Fraction { Numerator = 1, Denominator = 2 };
             var f2 = new DataTypes.Fraction { Numerator = 2, Denominator = 3 };
             try
             {
-                Console.WriteLine(f1[0]); 
-                Console.WriteLine(f1[1]); 
-                
                 Console.WriteLine($"{f1} + {f2} = {f1 + f2}");
                 Console.WriteLine($"{f1} - {f2} = {f1 - f2}");
                 Console.WriteLine($"{f1} * {f2} = {f1 * f2}");
